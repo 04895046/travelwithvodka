2fdbb7d6099f6aa52e6d6111e9d7e818a4706723
Jingtian Hu <jt.hu@mail.utoronto.ca>
2025-12-18 01:11:19 -0500

    update

--- backend/my_backend/settings.py
+++ backend/my_backend/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Must be before CommonMiddleware
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
